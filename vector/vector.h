ce9aa0275e1ef5fd9f00c9ae4a12bb5958e9b5ad
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-04-08 20:55:28 +0530

    commit

--- vector/vector.h
+++ vector/vector.h
@@ -5,32 +5,36 @@ typedef enum error_codes_vectors
 {
 	VECTOR_INIT_FAIL,
 	VECTOR_RESIZE_FAIL,
-	VECTOR_NO_SUCH_ELEMENT_FAIL,
-
+	NO_SUCH_ELEMENT_FAIL,
+	INVALID_ELEMENT_ARG
 }	errorVectors;
 
 typedef struct dynamic_arr
 {
 	void** arr;
-	uint64_t max_capacity;
-	uint64_t elements;
+	uint32_t max_capacity;
+	uint32_t elements;
 	
 	void (*destroy_fn)(void*);                               // function for freeing the data provided by user
 	const void* (*comparision_fn)(const void*, const void*); // returns a pointer to the greater element
 }	vector;
 
-errorVectors initVector(vector* vector1, uint64_t init_size, void (*destroy_fn)(void*), const void* (*comparision_fn)(const void*, const void*));
+errorVectors initVector(vector* vector1, uint32_t init_size, void (*destroy_fn)(void*), const void* (*comparision_fn)(const void*, const void*));
 
 static errorVectors resize(vector* vector1);
 
-errorVectors insert(vector* vector1, const void* element, uint64_t index);
+errorVectors insert(vector* vector1, const void* element, uint32_t index);
 
 errorVectors push(vector* vector1, const void* element);
 
-errorVectors remove(vector* vector1, const void* element, uint64_t index);
+errorVectors remove(vector* vector1, const void* element, uint32_t index);
 
 errorVectors pop(vector* vector1, const void* element);
 
-errorVectors 
+errorVectors clear(vector* vector1);
+
+errorVectors atIndex(vector* vector1, uint32_t index);
+
+errorVectors contains(vector* vector1, const void* element);
 
 #endif
