082ee25d0d970a91f3383ee8348354aba7c727ce
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-04-10 09:20:27 +0530

    commit_1

--- vector/vector.c
+++ vector/vector.c
@@ -3,4 +3,46 @@
 #include <stdint.h>
 #include <assert.h>
 #include <limits.h>
+#include <stdbool.h>
 
+vector* init(uint32_t init_size, void (*destructor)(void*))
+{
+	assert(init_size == 0 || init_size <= UINT32_MAX / 8);
+
+	vector* new_vector = (vector*)malloc(sizeof(vector));
+	
+	assert(new_vector != NULL);
+
+	new_vector->arr = (void**)calloc((size_t)(init_size), (size_t)sizeof(void*));
+
+	assert(new_vector->arr != NULL);
+
+	new_vector->capacity = init_size;
+	new_vector->elements = 0;
+	return new_vector;
+}
+
+void resize(vector* current_vector)
+{
+	if (current_vector->capacity == UINT32_MAX)
+	{
+		assert(current_vector->capacity <= UINT32_MAX);
+	}
+
+	uint32_t new_capacity = (current_vector->capacity > UINT32_MAX / 2 ? UINT32_MAX : (current_vector->capacity) * 2);
+
+	void** temp_ptr_resized_arr = (void**)realloc((void*)current_vector->arr, (size_t)(sizeof(void**) * new_capacity);
+
+	assert(temp_ptr_resized_arr != NULL);
+
+	current_vector->arr = temp_ptr_resized_arr;
+	temp_ptr_resized_arr = NULL;
+	current_vector->capacity = new_capacity;
+
+	return;
+}
+
+void shrink_to_fit(vector* current_vector)
+{
+	
+}
--- vector/vector.h
+++ vector/vector.h
@@ -1,55 +1,28 @@
 #ifndef VECTOR
 #define VECTOR
 
-typedef enum error_codes_vectors
-{
-	VECTOR_OK,
-	VECTOR_INIT_FAIL,
-	VECTOR_RESIZE_FAIL,
-	NO_SUCH_ELEMENT_FAIL,
-	INVALID_ELEMENT_ARG,
-
-
-}	errorVectors;
-
-const char* vector_error_messages[] = {
-	"No error!",
-	"Vector could not be initialized!",
-	"Vector could not be resized",
-	"No such Element exists!",
-	"Invalid argument given!"
-}
-
 typedef struct vector
 {
 	void** arr;
-	uint32_t max_capacity;
+	uint32_t capacity;
 	uint32_t elements;
-	
-	void (*destroy_fn)(void*);                               // function for freeing the data provided by user
-	const void* (*comparision_fn)(const void*, const void*); // returns a pointer to the greater element
 }	vector;
 
-vector* initVector(uint32_t init_size, void (*destroy_fn)(void*), const void* (*comparision_fn)(const void*, const void*));
+vector* init(uint32_t init_size, void (*destructor)(void*));
 
-static void resize(vector* vector1);
+void resize(vector* current_vector);
 
-void insert(vector* vector1, const void* element, uint32_t index);
+void shrink_to_fit(vector* current_vector);
 
-void push(vector* vector1, const void* element);
+void push(vector* current_vector, const void* element);
 
-const void* remove(vector* vector1, uint32_t index);
+void insert(vector* current_vector, const void* element, uint32_t index);
 
-const void* pop(vector* vector1);
+const void* pop(vector* current_vector);
 
-const void* clear(vector* vector1);
+const void* remove(vector* current_vector, uint32_t index);
 
-const void* atIndex(vector* vector1, uint32_t index);
+void destroy(vector* current_vector, void (*destroy)(void*));
 
-const void* contains(vector* vector1, const void* element);
-
-uint32_t capacity(vector* vector_1, uint32_t* capacity);
-
-
-
+const void* hasElement(vector* current_vector);   // return NULL if element not found
 #endif
