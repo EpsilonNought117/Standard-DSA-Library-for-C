ef01c418c3ad132772f7d9447d0bbc42cf6589e7
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-04-14 18:31:24 +0530

    fixed user defined main header

--- header_to_include/std_dsa_lib.h
+++ header_to_include/std_dsa_lib.h
@@ -5,4 +5,4 @@
 #include "Standard_DSA_Library_for_C/data_structures/lists/linked_lists/singly_linked_list/singlylinklist.h"
 #include "Standard_DSA_Library_for_C/data_structures/queue/queue.h"
 #include "Standard_DSA_Library_for_C/data_structures/stack/vector_stack.h"
-#include "Standard_DSA_Library_for_C\algorithms\sorting\sorting.h"
+#include "Standard_DSA_Library_for_C/algorithms/sorting/sorting.h"
