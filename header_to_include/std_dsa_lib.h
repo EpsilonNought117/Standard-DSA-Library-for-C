f2cabdcb5ad46ff1d44738bd604430635481cc9f
Devansh Mehrotra <devanshmehrotra117@gmail.com>
2024-04-15 15:50:37 +0530

    main header

--- header_to_include/std_dsa_lib.h
+++ header_to_include/std_dsa_lib.h
@@ -12,6 +12,7 @@
 #include "Standard_DSA_Library_for_C/data_structures/deque/deque.h"
 #include "Standard_DSA_Library_for_C/data_structures/lists/vector/vector.h"
 #include "Standard_DSA_Library_for_C/data_structures/lists/linked_lists/singly_linked_list/singlylinklist.h"
+#include "Standard_DSA_Library_for_C/data_structures/lists/linked_lists/doubly_linked_list/doublylinklist.h"
 #include "Standard_DSA_Library_for_C/data_structures/queue/queue.h"
 #include "Standard_DSA_Library_for_C/data_structures/stack/vector_stack.h"
 #include "Standard_DSA_Library_for_C/algorithms/sorting/sorting.h"
